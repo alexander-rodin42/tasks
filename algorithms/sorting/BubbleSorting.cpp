d063fb966851fbb2ce098595eda4ab517ff6924e
Alexander Rodin <Project177117@gmail.com>
2022-09-15 00:33:23 +0500

    fix: .clang-format

--- algorithms/sorting/BubbleSorting.cpp
+++ algorithms/sorting/BubbleSorting.cpp
@@ -3,71 +3,71 @@
 #include <vector>
 
 namespace rav {
-	void bubble_sorting(
-	    std::vector<int>& vector,
-	    std::function<bool(int, int)> comparator = [](int a, int b) -> bool { return a > b; }) noexcept
-	{
-		for (size_t i = 0; i < vector.size(); ++i)
-		{
-			for (size_t j = 0; j < vector.size() - i - 1; ++j)
-			{
-				if (comparator(vector[j], vector[j + 1]))
-				{
-					std::swap(vector[j], vector[j + 1]);
-				}
-			}
-		}
-	}
+    void bubble_sorting(
+        std::vector<int>& vector,
+        std::function<bool(int, int)> comparator = [](int a, int b) -> bool { return a > b; }) noexcept
+    {
+        for (size_t i = 0; i < vector.size(); ++i)
+        {
+            for (size_t j = 0; j < vector.size() - i - 1; ++j)
+            {
+                if (comparator(vector[j], vector[j + 1]))
+                {
+                    std::swap(vector[j], vector[j + 1]);
+                }
+            }
+        }
+    }
 
-	void print_vector(const std::vector<int>& vector, const std::string& message)
-	{
-		std::cout << message;
-		for (const auto& value : vector)
-		{
-			std::cout << value << " ";
-		}
-		std::cout << std::endl;
-	}
+    void print_vector(const std::vector<int>& vector, const std::string& message)
+    {
+        std::cout << message;
+        for (const auto& value : vector)
+        {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
+    }
 
-	bool custom_comparator(int a, int b)
-	{
-		return a < b;
-	}
+    bool custom_comparator(int a, int b)
+    {
+        return a < b;
+    }
 
-	//----------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
 
-	template <typename Collection, typename Comparator, typename = typename Collection::size_type>
-	void bubble_sorting_template(Collection& collection, Comparator comparator) noexcept
-	{
-		for (typename Collection::size_type i = 0; i < collection.size(); i++)
-		{
-			for (typename Collection::size_type j = 0; j < collection.size() - i - 1; j++)
-			{
-				if (comparator(collection[j], collection[j + 1]))
-				{
-					std::swap(collection[j], collection[j + 1]);
-				}
-			}
-		}
-	}
+    template <typename Collection, typename Comparator, typename = typename Collection::size_type>
+    void bubble_sorting_template(Collection& collection, Comparator comparator) noexcept
+    {
+        for (typename Collection::size_type i = 0; i < collection.size(); i++)
+        {
+            for (typename Collection::size_type j = 0; j < collection.size() - i - 1; j++)
+            {
+                if (comparator(collection[j], collection[j + 1]))
+                {
+                    std::swap(collection[j], collection[j + 1]);
+                }
+            }
+        }
+    }
 }  // namespace rav
 
 
 int main()
 {
-	std::vector<int> vector = {7, 9, 1, 5, 8, 1, 8, 3, 7, 3, 4, 1, 9, 2, 3};
-	rav::print_vector(vector, "Not sorted array: ");
+    std::vector<int> vector = {7, 9, 1, 5, 8, 1, 8, 3, 7, 3, 4, 1, 9, 2, 3};
+    rav::print_vector(vector, "Not sorted array: ");
 
-	rav::bubble_sorting(vector);
-	rav::print_vector(vector, "Sorted array: ");
+    rav::bubble_sorting(vector);
+    rav::print_vector(vector, "Sorted array: ");
 
-	rav::bubble_sorting(vector, rav::custom_comparator);
-	rav::print_vector(vector, "Sorted array: ");
+    rav::bubble_sorting(vector, rav::custom_comparator);
+    rav::print_vector(vector, "Sorted array: ");
 
-	//----------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
 
-	rav::bubble_sorting_template(vector, [](int a, int b) -> bool { return a > b; });
-	rav::print_vector(vector, "Sorted array: ");
+    rav::bubble_sorting_template(vector, [](int a, int b) -> bool { return a > b; });
+    rav::print_vector(vector, "Sorted array: ");
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
--- alignment/main.cpp
+++ alignment/main.cpp
@@ -2,53 +2,53 @@
 
 struct S1
 {
-	int i : 3;
-	int j : 5;
+    int i : 3;
+    int j : 5;
 };
 
 struct S2
 {
-	int i;
-	int j;
+    int i;
+    int j;
 };
 
 struct S3
 {
-	bool i;
-	bool j;
-	bool k;
+    bool i;
+    bool j;
+    bool k;
 };
 
 struct S4
 {
-	bool i;
-	bool j;
-	bool k;
-	bool z;
+    bool i;
+    bool j;
+    bool k;
+    bool z;
 };
 
 struct S5
 {
-	int i;
-	bool j;
+    int i;
+    bool j;
 };
 
 struct S6
 {
-	double i;
-	bool j;
+    double i;
+    bool j;
 };
 
 int main()
 {
-	std::cout << alignof(std::max_align_t) << " - max align." << std::endl;
-	std::cout << "align: " << alignof(S1) << ", size: " << sizeof(S1)
-	          << " - int : 3 + int : 5 (length in bits for each field)" << std::endl;
-	std::cout << "align: " << alignof(S2) << ", size: " << sizeof(S2) << " - 2*int" << std::endl;
-	std::cout << "align: " << alignof(S3) << ", size: " << sizeof(S3) << " - 3*bool" << std::endl;
-	std::cout << "align: " << alignof(S4) << ", size: " << sizeof(S4) << " - 4*bool" << std::endl;
-	std::cout << "align: " << alignof(S5) << ", size: " << sizeof(S5) << " - int + bool" << std::endl;
-	std::cout << "align: " << alignof(S6) << ", size: " << sizeof(S6) << " - double + bool" << std::endl;
+    std::cout << alignof(std::max_align_t) << " - max align." << std::endl;
+    std::cout << "align: " << alignof(S1) << ", size: " << sizeof(S1)
+              << " - int : 3 + int : 5 (length in bits for each field)" << std::endl;
+    std::cout << "align: " << alignof(S2) << ", size: " << sizeof(S2) << " - 2*int" << std::endl;
+    std::cout << "align: " << alignof(S3) << ", size: " << sizeof(S3) << " - 3*bool" << std::endl;
+    std::cout << "align: " << alignof(S4) << ", size: " << sizeof(S4) << " - 4*bool" << std::endl;
+    std::cout << "align: " << alignof(S5) << ", size: " << sizeof(S5) << " - int + bool" << std::endl;
+    std::cout << "align: " << alignof(S6) << ", size: " << sizeof(S6) << " - double + bool" << std::endl;
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
