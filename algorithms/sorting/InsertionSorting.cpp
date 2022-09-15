5bd146e9426c3f79997c7204c65560399394219d
Alexander Rodin <Project177117@gmail.com>
2022-09-16 00:22:48 +0500

    add insertion sorting

---
+++ algorithms/sorting/InsertionSorting.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+namespace rav {
+
+    auto custom_comparator = [](int a, int b) -> bool { return a > b; };
+
+    void insertion_sorting(std::vector<int>& vector, std::function<bool(int, int)> comparator = custom_comparator) noexcept
+    {
+        for (int i = 1; i < vector.size(); ++i)
+        {
+            for (int j = i; (j > 0) && comparator(vector[j - 1], vector[j]); --j)
+            {
+                std::swap(vector[j - 1], vector[j]);
+            }
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    template <typename Collection, typename Comparator>
+    void insertion_sorting_template(Collection& collection, Comparator comparator) noexcept
+    {
+        for (auto i = 1; i < collection.size(); i++)
+            for (auto j = i; (j > 0) && (comparator(collection[j - 1], collection[j])); j--)
+                std::swap(collection[j - 1], collection[j]);
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    template <typename Collection>
+    void print_collection(const Collection& collection, const std::string& message)
+    {
+        std::cout << message;
+        for (const auto& value : collection)
+        {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
+    }
+
+}  // namespace rav
+
+
+int main()
+{
+    std::vector<int> vector = {7, 9, 1, 5, 8, 1, 8, 3, 7, 3, 4, 1, 9, 2, 3};
+    rav::print_collection(vector, "Not sorted vector: ");
+
+    rav::insertion_sorting(vector);
+    rav::print_collection(vector, "Sorted vector: ");
+
+    //----------------------------------------------------------------------------------------------
+
+    rav::insertion_sorting_template(vector, [](int a, int b) -> bool { return a < b; });
+    rav::print_collection(vector, "Sorted vector: ");
+
+    return EXIT_SUCCESS;
+}
